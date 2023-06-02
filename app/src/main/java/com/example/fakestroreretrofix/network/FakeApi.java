da86400e325051a8b6e11fb0ee692cb10e8ff333
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-06-02 15:27:31 +0530

    Made changes in design part

--- app/src/main/java/com/example/fakestroreretrofix/network/FakeApi.java
+++ app/src/main/java/com/example/fakestroreretrofix/network/FakeApi.java
@@ -1,14 +1,27 @@
 package com.example.fakestroreretrofix.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.HttpException;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FakeApi {
     public FakeApiService fakeApiService(){
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://fakestoreapi.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         return retrofit.create(FakeApiService.class);
     }
 }
+
